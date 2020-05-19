044cef757bed7bba8fa1174c40064921838b41ee
Hudson R <hudson.santosr@gmail.com>
2020-05-19 15:42:08 -0300

    feature: upload files to repo

---
+++ src/server.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+var pjson = require('../package.json')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function (req, res) {
+  res.send(`Extrack Crawler v${pjson.version}`)
+})
+
+require('./api/routes/item')(app)
+
+app.disable('x-powered-by')
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Aplicação Node rodando na porta: ${port}.`)
+})
