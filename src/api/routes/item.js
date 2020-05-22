ec90f931d6f6cb1f5057bda0c94104ef6bf537b8
Hudson R <hudson.santosr@gmail.com>
2020-05-21 22:50:23 -0300

    revert: commit sonda

--- src/api/routes/item.js
+++ src/api/routes/item.js
@@ -7,42 +7,79 @@ const router = express.Router()
 router.post('/item', async (req, res) => {
   const { name } = req.body
 
-  try {
-    // Sonda
-    let beertoList = []
-    await axios
-      .get(`https://www.sondadelivery.com.br/delivery/categoria/${name}`)
-      .then(
-        (res) => {
-          if (res.status === 200) {
-            const html = res.data
+  switch (name.toLowerCase()) {
+    case 'beer':
+      try {
+        // Sonda
+        let beertoList = []
+        await axios
+          .get('https://www.sondadelivery.com.br/delivery/categoria/Cerveja')
+          .then(
+            (res) => {
+              if (res.status === 200) {
+                const html = res.data
 
-            const $ = cheerio.load(html)
+                const $ = cheerio.load(html)
 
-            $('.product').each(function (i, elem) {
-              beertoList[i] = {
-                store: 'Sonda',
-                title: $(this).find('.tit').text(),
-                url: `https://www.sondadelivery.com.br${$(this)
-                  .find('.js-link-produto')
-                  .attr('href')}`,
-                price: $(this).find('.int').text().trim(),
+                $('.product').each(function (i, elem) {
+                  beertoList[i] = {
+                    store: 'Sonda',
+                    title: $(this).find('.tit').text(),
+                    url: `https://www.sondadelivery.com.br${$(this)
+                      .find('.js-link-produto')
+                      .attr('href')}`,
+                    price: $(this).find('.int').text().trim(),
+                  }
+                })
               }
-            })
-          }
-        },
-        (err) => console.log(err)
-      )
+            },
+            (err) => console.log(err)
+          )
 
-    const beertoListTrimmed = beertoList.filter((n) => n != undefined)
+        // Sempre em casa
+        await axios.get('https://sempreemcasa.com.br/collections/cerveja').then(
+          (res) => {
+            if (res.status === 200) {
+              const html = res.data
 
-    // Response
-    return res.send(JSON.stringify(beertoListTrimmed, null, 4))
-  } catch (err) {
-    res.status(400).send({ erro: 'Oops! Encontramos uma falha!' })
+              const $ = cheerio.load(html)
+
+              $('.product-item.card').each(function (i, elem) {
+                beertoList[i] = {
+                  store: 'Sempre em casa',
+                  title: $(this).find('.product-item__title-text').text(),
+                  url: `https://sempreemcasa.com.br${$(this)
+                    .find('.product-link')
+                    .attr('href')}`,
+                  price: $(this)
+                    .find('.unity__text')
+                    .first()
+                    .text()
+                    .replace('R$ ', '')
+                    .replace(' ', ''),
+                }
+              })
+            }
+          },
+          (err) => console.log(err)
+        )
+
+        const beertoListTrimmed = beertoList.filter((n) => n != undefined)
+
+        // Response
+        return res.send(JSON.stringify(beertoListTrimmed, null, 4))
+      } catch (err) {
+        res.status(400).send({ erro: 'Oops! Encontramos uma falha!' })
+      }
+
+      break
+
+    default:
+      return res
+        .status(404)
+        .send({ erro: 'Oops! Não encontramos nada melhor!' })
+      break
   }
-
-  return res.status(404).send({ erro: 'Oops! Não encontramos nada melhor!' })
 })
 
 module.exports = (app) => app.use('/find', router)
